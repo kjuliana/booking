d259b9677daae2a5955af26cb15fcd5315f644ec
Juliana Koroleva <korjuliana@gmail.com>
2023-05-10 01:38:13 +0300

    time and date optimization

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -19,11 +19,18 @@ const timeToString = (date: Date) => {
 }
 
 
-const getEndTime = (startTime: string, intervalMs: number): string => {
+const getEndTime = (startTime: string, startDate: string, intervalMs: number): Date => {
     const [hour, minute] = startTime.split(':');
-    const date = new Date();
-    date.setHours(Number(hour), Number(minute));
-    return timeToString(new Date(date.getTime() + intervalMs));
+    const [year, month, day] = startDate.split('-');
+    const date = new Date(
+        Number(year),
+        Number(month)- 1,
+        Number(day),
+        Number(hour),
+        Number(minute)
+    );
+
+    return new Date(date.getTime() + intervalMs);
 }
 
 const getDiffMs = (start: Date, end: Date): number => {
@@ -35,110 +42,143 @@ const TIME_STEP_MS = 1000*60*15;
 const Form = () => {
 
     const initialStart = new Date(Math.ceil(Date.now() / TIME_STEP_MS) * TIME_STEP_MS);
+    const initialEnd = new Date (initialStart.getTime() + 30 * 60 * 1000);
 
     const initialFormState:  IFormData = {
         tower: 'A',
         floor: '1',
         room: '1',
-        date: dateToString(initialStart),
+        dateStart: dateToString(initialStart),
+        dateEnd: dateToString(initialEnd),
         timeStart: timeToString(initialStart),
-        timeEnd: timeToString(new Date (initialStart.getTime() + 30 * 60 * 1000)),
+        timeEnd: timeToString(initialEnd),
         comment: '',
     }
 
     const [formState, setFormState] = useState(initialFormState);
 
+    const intervalMs = getDiffMs(
+        new Date(formState.dateEnd + ' ' + formState.timeEnd),
+        new Date(formState.dateStart + ' ' + formState.timeStart)
+    );
+
+    const intervalHours = Math.floor(intervalMs/ 1000/ 60 / 60);
+    const intervalMinutes = intervalMs/1000/ 60 % 60;
+
     useEffect(() => {
-        const newTimeEnd = getEndTime(formState.timeStart,30 * 60 * 1000);
-        setFormState(prevFormState => ({...prevFormState, timeEnd: newTimeEnd}));
-    }, [formState.timeStart, formState.date])
+        const newStart = (new Date(formState.dateStart + ' ' + formState.timeStart));
+        if (initialStart.getTime() > newStart.getTime()) {
+            setFormState(prevFormState => ({
+                ...prevFormState,
+                timeStart: initialFormState.timeStart,
+                timeEnd: initialFormState.timeEnd,
+                dateStart: initialFormState.dateStart,
+                dateEnd: initialFormState.dateEnd
+            }))
+        } else {
+            const newEnd = getEndTime(formState.timeStart, formState.dateStart, 1000*60*30);
+            const newTimeEnd = timeToString(newEnd);
+            const newDateEnd = dateToString(newEnd);
+            setFormState(prevFormState => ({...prevFormState, timeEnd: newTimeEnd, dateEnd: newDateEnd}));
+        }
+    }, [formState.timeStart, formState.dateStart])
 
+    useEffect(() => {
+        const [newEndHour, newEndMinute] = formState.timeEnd.split(':');
+        const [startHour, startMinute] = formState.timeStart.split(':');
+        const [year, month, day] = formState.dateEnd.split('-');
 
-    let intervalMinutes = getDiffMs(
-        new Date(formState.date + ' ' + formState.timeEnd),
-        new Date(formState.date + ' ' + formState.timeStart)
-    )/1000/60;
-
-    let intervalHours = 0;
-
-    if (intervalMinutes > 59) {
-        intervalHours = Math.floor(intervalMinutes / 60);
-        intervalMinutes = intervalMinutes % 60;
-    }
+        if (startHour < newEndHour || (newEndHour === startHour && newEndMinute > startMinute)) {
+            setFormState(prevFormState => ({...prevFormState, dateEnd: formState.dateStart}));
+        } else if (formState.dateStart === formState.dateEnd) {
+            const newEndDate =  new Date(new Date(
+                Number(year),
+                Number(month)- 1,
+                Number(day),
+                Number(newEndHour),
+                Number(newEndMinute)
+            ).getTime() + 1000*60*60*24);
+            setFormState(prevFormState => ({...prevFormState, dateEnd: dateToString(newEndDate)}));
+        }
+    }, [formState.timeEnd])
 
     return (
         <div className={styles.root}>
-            <h1>Бронирование переговорных комнат</h1>
+            <h1 className={styles.title}>Бронирование переговорки</h1>
             <div className={styles.content}>
-                <div className={styles.column}>
-                    <FieldSelect
-                        currentValue={formState.tower}
-                        key={'tower'}
-                        id={'tower'}
-                        onChange={(newValue) => setFormState({...formState, tower: newValue})}
-                        options={['A', 'B']}
-                        title={'Башня'}
-                    />
-                    <FieldSelect
-                        currentValue={formState.floor}
-                        key={'floor'}
-                        id={'floor'}
-                        onChange={(newValue) => setFormState({...formState, floor: newValue})}
-                        options={['3', '4', '5', '6', '7', '8']}
-                        title={'Этаж'}
-                    />
-                    <FieldSelect
-                        currentValue={formState.room}
-                        key={'room'}
-                        id={'room'}
-                        onChange={(newValue) => setFormState({...formState, room: newValue})}
-                        options={['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']}
-                        title={'Переговорка'}
-                    />
+                    <div className={styles.themeWrapper}>
+                        <h3 className={styles.themeTitle}>Где</h3>
+                        <FieldSelect
+                            currentValue={formState.tower}
+                            key={'tower'}
+                            id={'tower'}
+                            onChange={(newValue) => setFormState({...formState, tower: newValue})}
+                            options={['A', 'B']}
+                            title={'Башня'}
+                        />
+                        <FieldSelect
+                            currentValue={formState.floor}
+                            key={'floor'}
+                            id={'floor'}
+                            onChange={(newValue) => setFormState({...formState, floor: newValue})}
+                            options={['3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15',
+                                '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27']}
+                            title={'Этаж'}
+                        />
+                        <FieldSelect
+                            currentValue={formState.room}
+                            key={'room'}
+                            id={'room'}
+                            onChange={(newValue) => setFormState({...formState, room: newValue})}
+                            options={['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']}
+                            title={'Переговорка'}
+                        />
+                    </div>
+                    <div className={styles.themeWrapper}>
+                        <h3 className={styles.themeTitle}>Когда</h3>
+                        <div>
+                            <input
+                                className={styles.dateInput}
+                                id={'date'}
+                                type='date'
+                                value={formState.dateStart}
+                                min={dateToString(initialStart)}
+                                onChange={(e) => setFormState({...formState, dateStart: e.target.value})}
+                            />
+                            {formState.dateStart !== formState.dateEnd &&
+                            <span> ➜ {formState.dateEnd.split('-').reverse().join('.')}</span>
+                            }
+                        </div>
+                        <div>
+                            <input
+                                className={styles.timeInput}
+                                id={'timeStart'}
+                                type='time'
+                                value={formState.timeStart}
+                                onChange={(e) => setFormState({...formState, timeStart: e.target.value})}
+                                step={900}
+                            />
+                            <span> ➜ </span>
+                            <input
+                                className={styles.timeInput}
+                                id={'timeEnd'}
+                                type='time'
+                                value={formState.timeEnd}
+                                onChange={(e) => setFormState({...formState, timeEnd: e.target.value})}
+                                step={900}
+                            />
+                            <span>
+                                {intervalHours > 0 && ' ' + intervalHours + ' ч. '}
+                                    {' ' + intervalMinutes} мин.
+                            </span>
+                        </div>
+                    </div>
                     <TextArea
                         value={formState.comment}
                         id={'comment'}
                         onChange={(newValue) => setFormState({...formState, comment: newValue})}
-                        placeholder={'Оставьте комментарий'}
+                        placeholder={'Комментарий'}
                     />
-                </div>
-                <div className={styles.column}>
-                    <input
-                        className={styles.dateInput}
-                        id={'date'}
-                        type='date'
-                        value={formState.date}
-                        min={dateToString(initialStart)}
-                        onChange={(e) => setFormState({...formState, date: e.target.value})}
-                    />
-                    <div>
-                        <input
-                            className={styles.timeInput}
-                            id={'timeStart'}
-                            type='time'
-                            value={formState.timeStart}
-                            onChange={(e) => {
-                                setFormState({...formState, timeStart: e.target.value})
-                            }}
-                            step={900}
-                        />
-                        <span> - </span>
-                        <input
-                            className={styles.timeInput}
-                            id={'timeEnd'}
-                            type='time'
-                            value={formState.timeEnd}
-                            onChange={(e) => {
-                                setFormState({...formState, timeEnd: e.target.value})
-                            }}
-                            step={900}
-                        />
-                        <span>
-                            {intervalHours > 0 && ' ' + intervalHours + ' ч. '}
-                            {' ' + intervalMinutes} мин.
-                        </span>
-                    </div>
-                </div>
             </div>
             <div className={styles.actions}>
                 <Button
